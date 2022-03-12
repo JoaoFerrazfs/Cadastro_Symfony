c15642f62124c8af2ca69511c592a260cff9cafa
JoaoFerrazfs <joaoferrazp@gmail.com>
2022-03-12 17:55:37 -0300

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DefaultController
+{
+    
+    // public function index(Request $request)
+    // {
+
+
+    //     $resp = new Response();
+    //     $resp->setContent(json_encode(
+    //         [
+    //             "recebido" => $request->get('nome'),
+    //             "ip" => $request->getClientIp()
+
+    //         ]
+    //     ));
+    //     $resp->setStatusCode(200);
+
+    //     return $resp;
+    // }
+}
